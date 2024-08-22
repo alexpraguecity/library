87e2d9945e131c8a36f5d7f460012ec9972b85d7
Alex <alexpragueversion@gmail.com>
2024-08-22 20:09:35 +0200

    revert

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const express = require('express'); //for importing express
 const app = express(); //initializes express
 
 app.use(express.json()); //this is used to parse requests
-//testing
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 //starts the server
 app.listen(process.env.PORT, () => {
